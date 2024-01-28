a129505dbaf7a22c49575cbcf798bfa71c3550cb
innocentstudentgithub <157974336+innocentstudentgithub@users.noreply.github.com>
2024-01-28 21:52:16 +0530

    Create math_game.py

---
+++ math_game.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEM = 10
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operators = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operators + " " + str(right)
+    answer = eval(expr)
+    # "eval" function remove all if else statement
+    return expr, answer
+
+wrong = 0
+input("Press enter to start!")
+print(".....................")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEM):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("problem N" + str( i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong == 1
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print(".....................")
+print("Nice Work! You finished in", total_time, "seconds!")
